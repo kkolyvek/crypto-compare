4311795d52e2c2902497a18f2e76b3d2735b7105
Koppi Kolyvek <kk674@cornell.edu>
2021-07-06 11:36:28 -0700

    made an input field and some lines of javascript for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,44 @@
 
-var currentVal = $("#currentVal")
-var summary = $("#adlInfo")
+// ****************
+// GLOBAL VARIABLES
+// ****************
 
+var currentVal = $("#currentVal");
+var summary = $("#adlInfo");
+
+var currency1 = '';
+var currency2 = 'USD';
+
+// CoinGecko API
+var coinGeckoBase = 'https://api.coingecko.com/api/v3/';
+
+
+// *********
+// FUNCTIONS
+// *********
+
+function retrievePrice1(api) {
+    fetch(api)
+        .then(response => response.json)
+        .then(function (data) {
+            console.log(data)
+        });
+};
+
+
+
+// ***************
+// EVENT LISTENERS
+// ***************
+$('#currency1form').on('submit', function(event) {
+    event.preventDefault();
+
+    // alert user if input is invalid
+
+    // ping API using user input
+    var userInput1 = $('#currency1-input').val();
+
+    var apiCall = coinGeckoBase + 'coins/' + userInput1;
+    console.log(apiCall)
+    retrievePrice1(apiCall);
+});
