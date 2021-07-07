3b5f9c7163c2c403ce1b22cd36624b68d3e79562
Koppi Kolyvek <kk674@cornell.edu>
2021-07-06 17:37:41 -0700

    added very basic functionality to converter input using the CoinGecko price API

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,26 @@ var currency2 = 'USD';
 
 // CoinGecko API
 var coinGeckoBase = 'https://api.coingecko.com/api/v3/';
+var coinObject = {};
+// fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=false')
+//     .then(response => response.json())
+//     .then(function (data) {
+//         coinObject = data;
+//         console.log(data);
+//     });
 
 
 // *********
 // FUNCTIONS
 // *********
 
-function retrievePrice1(api) {
+function retrievePrice(api, first, second) {
     fetch(api)
         .then(response => response.json())
         .then(function (data) {
-            console.log(data.tickers[0].last)
-            $('#result').text(`1 ${data.id} is ${data.tickers[0].last} USD`);
+            var vs_currencies = data[first];
+            var val = (vs_currencies[second])
+            $('#second-currency-amount').val(val);
         });
 };
 
@@ -31,25 +39,29 @@ function retrievePrice1(api) {
 // ***************
 // EVENT LISTENERS
 // ***************
-$('#currency1form').on('submit', function(event) {
+$('#currency1-input').bind('change', function(event) {
     event.preventDefault();
 
     // alert user if input is invalid
 
     // ping API using user input
     var userInput1 = $('#currency1-input').val();
+    var userInput2 = $('#currency2-input').val();
 
-    var apiCall = coinGeckoBase + 'coins/' + userInput1 + '?localization=false';
+    var apiCall = coinGeckoBase + 'simple/price?ids=' + userInput1 + '&vs_currencies=' + userInput2;
     console.log(apiCall)
-    retrievePrice1(apiCall);
+    retrievePrice(apiCall, userInput1, userInput2.toLowerCase());
 });
 
-
+// materialize listeners
 $(document).ready(function(){
    $('select').formSelect();
 });
 
 // *******
-// ON-LOAD
+// ON LOAD
 // *******
-$('#currency2-input').val('USD')
+
+// default inputs
+$('#first-currency-amount').val(1);
+$('#currency2-input').val('USD');
